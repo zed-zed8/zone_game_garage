9d6087c5c206bae11f2f51b01beb1bad23b45746
zed-zed8 <ziddanm45@gmail.com>
2026-09-12 08:17:18 +0700

    test

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -21,7 +21,7 @@ class User {
       'username': username,
       'email': email,
       'password': password,
-      'created_at': createdAt?.toIso8601String(),
+      'created_at': createdAt!.toIso8601String(),
     };
   }
 
